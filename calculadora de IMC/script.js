02b615fe62998130379f07413fbc23ebada13e0a
Leonardo Amichi <leoamichi1801@gmail.com>
2024-09-09 18:06:53 -0300

    .

--- calculadora de IMC/script.js
+++
@@ -1,9 +0,0 @@
-
-function valor() {
-    var peso, altura, imc
-    peso = document.getElementById("peso").value
-    altura = document.getElementById("altura").value
-    imc = peso / (altura * altura)
-    document.getElementById ("resultado").innerHTML = "IMC: " + imc
-    
-}
